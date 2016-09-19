d7694c587bc5304adf3aa3050894bffd7e584b61
huawei <460706836@qq.com>
2016-09-19 21:16:06 +0800

    FileOperator, provide open, sava and sava as

---
+++ FileOperator/ImageFileOperator.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "common/common.h"
+#include <string>
+
+class Image;
+class ImageFileOperator
+{
+	DISALLOW_COPY_AND_ASSIGN(ImageFileOperator);
+public:
+	ImageFileOperator() = default;
+	~ImageFileOperator() = default;
+
+	virtual std::string Open() = 0;
+	virtual bool Save(const Image& img, const std::string& filename) = 0;
+	virtual bool SaveAs(const Image& img) = 0;
+};
---
+++ FileOperator/QImageFileOperator.cpp
@@ -0,0 +1,56 @@
+#include "FileOperator/QImageFileOperator.h"
+
+#include "DataStructure/Image.h"
+#include "ImageChanger/ToQImage.h"
+#include <QImage>
+#include <QString>
+#include <QFileDialog>
+#include <QObject>
+#include <QMessageBox>
+
+#include <cassert>
+
+std::string QImageFileOperator::Open()
+{
+	QString filename = QFileDialog::getOpenFileName(
+		nullptr, QObject::tr("Open Image"), ".", QObject::tr("Image File(*.png *.jpg *.jpeg *.bmp)"));
+	return filename.toStdString();
+}
+
+bool QImageFileOperator::Save(const Image& img, const std::string& filename)
+{
+	assert(filename != "");
+
+	QImage qImg;
+	ToQImage::DoChange(img, qImg);
+	qImg.save(QString::fromStdString(filename));
+	return true;
+}
+
+bool QImageFileOperator::SaveAs(const Image& img)
+{
+	QImage qImg;
+	ToQImage::DoChange(img, qImg);
+
+	QString filename = QFileDialog::getSaveFileName(nullptr,
+		QObject::tr("Save Image"),
+		"",
+		QObject::tr("*.bmp;; *.png;; *.jpg;; *.tif;; *.GIF")); //select path
+
+	if (filename.isEmpty())
+	{
+		return false;
+	}
+	else
+	{
+		if (!(qImg.save(filename))) //±£´æÍ¼Ïñ  
+		{
+			QMessageBox::information(nullptr,
+				QObject::tr("Failed to save the image"),
+				QObject::tr("Failed to save the image!"));
+			return false;
+		}
+	}
+	return true;
+}
+
