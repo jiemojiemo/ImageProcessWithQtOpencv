1ff2c4b71ed925b2b234af21146318090e3683b9
cjhkeep <593056806@qq.com>
2016-09-27 14:18:34 +0800

    add Contourer2.*

---
+++ Magic/Contourer2.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Magic/Magician.h"
+
+class Contourer2 : public Magician
+{
+	DISALLOW_COPY_AND_ASSIGN(Contourer2);
+public:
+	Contourer2() = default;
+	~Contourer2() = default;
+
+	virtual void DoMagic(Image& img);
+};
+
